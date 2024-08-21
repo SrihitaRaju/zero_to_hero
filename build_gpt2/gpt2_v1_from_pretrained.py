237fe39b5da52364c5038044e265a3da4498e6be
Srihita Vatsavaya <srihitavatsavaya@Srihitas-MBP.cable.rcn.com>
2024-08-21 16:54:48 -0500

    prelim checkpoint

--- build_gpt2/gpt2_v1_from_pretrained.py
+++ build_gpt2/gpt2_v1_from_pretrained.py
@@ -300,7 +300,7 @@ raw_model = model.module if ddp else model
 
 max_lr = 6e-4
 min_lr = 0.1 * max_lr
-max_steps = 19073 #10B / 2**19(0.5M)
+max_steps = 19073 #10B / 2**19(0.5M) # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
 warmup_steps = 715
 def get_lr(it):
     if it < warmup_steps:
@@ -348,6 +348,17 @@ for step in range(max_steps):
             print(f"validation loss: {val_loss_accum.item():.4f}")
             with open(log_file, 'a') as f:
                 f.write(f"{step} val {val_loss_accum.item():.4f}\n")
+                if step > 0 and (step % 5000 == 0 or last_step):
+                    checkpoint_path = os.path.join(log_dir, f"model_{step:05d}.pt")
+                    checkpoint = {
+                        'model': raw_model.state_dict(),
+                        'config' : raw_model.config,
+                        'step' : step,
+                        'val_loss': val_loss_accum.item()
+                    }
+                    # you might also want to add optimizer.state_dict() and
+                    # rng seeds etc., if you wanted to more exactly resume training
+                    torch.save(checkpoint, checkpoint_path)
 
 
     # once in a while generate from the model (except step 0, which is noise)
